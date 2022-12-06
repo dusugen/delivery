5a2e4ba078aed3af3b8d937011eda9308a9e1042
Kirill Malina <sailorxd1@mail.ru>
2022-12-06 16:54:12 +0400

    added map with geolocation

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,31 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+function GetMap() {
+  let map = new Microsoft.Maps.Map("#myMap");
+  map.setView({
+    zoom: 17,
+  });
+  let pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
+  map.entities.push(pushpin);
+  return map;
+}
+function centerMap(map) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      map.setView({
+        mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+        center: new Microsoft.Maps.Location(lat, long),
+      });
+    },
+    function (error) {
+      alert(JSON.stringify(error));
+    },
+    {
+      enableHighAccuracy: true,
+    }
+  );
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-
-	// Custom JS
-
-})
+window.addEventListener("load", () => {
+  centerMap(GetMap());
+});
